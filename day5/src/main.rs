c9b7ba5b1e74214271c520cee1704d510394a988
Simon Rapilly <simon@rapilly.com>
2018-12-05 16:42:55 +0100

    Day 5 Use part 1 polymer as a starting point for part 2
    run time divided by 4

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -2,16 +2,17 @@ use std::collections::VecDeque;
 
 fn main() {
     let input = include_str!("input.txt");
-    star_one(input);
-    star_two(input);
+    let reduced_polymer = star_one(input);
+    star_two(reduced_polymer);
 }
 
-fn star_one(input: &str) {
+fn star_one(input: &str) -> VecDeque<char> {
     let mut deque: VecDeque<char> = VecDeque::new();
     for c in input.chars() {
         update_deque(&mut deque, c)
     }
     println!("Day 5 - Part 1 : {}", deque.len());
+    return deque;
 }
 
 fn update_deque(deque: &mut VecDeque<char>, c: char) -> () {
@@ -30,7 +31,7 @@ fn is_react(x: char, y: char) -> bool {
         || y.is_ascii_uppercase() && x.is_ascii_lowercase());
 }
 
-fn star_two(input: &str) {
+fn star_two(input: VecDeque<char>) {
     let alphabet = ['a', 'b', 'c', 'd', 'e', 'f',
         'g', 'h', 'i', 'j', 'k', 'l',
         'm', 'n', 'o', 'p', 'q', 'r',
@@ -40,9 +41,9 @@ fn star_two(input: &str) {
     let result = alphabet.iter()
         .map(|char_to_remove| {
             let mut deque:  VecDeque<char> = VecDeque::new();
-            for c in input.chars() {
+            for c in input.iter() {
                 if !c.eq_ignore_ascii_case(char_to_remove) {
-                    update_deque(&mut deque, c)
+                    update_deque(&mut deque, *c)
                 }
             }
             deque.len()
