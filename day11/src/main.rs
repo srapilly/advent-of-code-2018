805c54afa66ec58b7f04b128b2d9d1909051187f
Simon Rapilly <simon@rapilly.com>
2018-12-22 17:17:08 +0100

    Day 11

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,12 +1,35 @@
 fn main() {
+    part_one();
+    part_two();
+}
+
+fn part_one() {
+    let result = get_square(16);
+    println!("{:?}", result);
+}
+
+fn part_two() {
+    let mut max = (0, 0, (0, 0));
+    //really slow !!
+    for i in 1..20 {
+        println!("{}", i);
+        let (size, max_power_level, max_power_level_coord) = get_square(i);
+        if max_power_level > max.1 {
+            max = (size, max_power_level, max_power_level_coord);
+        }
+    }
+    println!("{:?}", max);
+}
+
+fn get_square(size: i32) -> (i32, i32, (i32, i32)) {
     let input = 9110;
     let mut max_power_level = 0;
     let mut max_power_level_coord = (0, 0);
-    for x in 1..300 - 3 {
-        for y in 1..300 - 3 {
+    for x in 1..300 - size {
+        for y in 1..300 - size {
             let mut power_level = 0;
-            for a in x..x + 3 {
-                for b in y..y + 3 {
+            for a in x..x + size {
+                for b in y..y + size {
                     power_level = power_level + get_cell_power_level((a, b), input);
                 }
             }
@@ -16,10 +39,10 @@ fn main() {
             }
         }
     }
-    println!("{} {:?}", max_power_level, max_power_level_coord)
+    (size, max_power_level, max_power_level_coord)
 }
 
-fn get_cell_power_level((x, y): (usize, usize), serial_number: usize) -> i32 {
+fn get_cell_power_level((x, y): (i32, i32), serial_number: i32) -> i32 {
     let rack_id = x + 10;
     let power_level_start = rack_id * y;
     let add_serial_number = power_level_start + serial_number;
